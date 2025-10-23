a3fffdf88ffcac7f2a0932da141e1e232a3cf51d
Mark Shikalov <faster03w@gmail.com>
2025-10-23 02:27:28 +0200

    Add files via upload

---
+++ l4(1).cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    // ПУНКТ 1
+    int a[11];
+
+    cout << "Введите 11 чисел: ";
+    for (int i = 0; i < 11; i++) {
+        cin >> a[i];
+    }
+
+    // Сумма первых трёх
+    int s = a[0] + a[1] + a[2];
+
+    // Если сумма > 100 — сортируем
+    if (s > 100) {
+        // СОРТИРОВКА
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
+        }
+    }
+
+    // Вывод массива
+    for (int i = 0; i < 11; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl << endl;
+
+    // ПУНКТ 2: матрица 3x4
+    int m[3][4];
+
+    cout << "Введите 12 чисел для матрицы 3x4: ";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> m[i][j];
+        }
+    }
+
+    // Найдём столбец с самой большой (наименее отрицательной) суммой отрицательных чисел
+    int stolb = -1;
+    int bsum = -1000000; // очень маленькое число
+
+    for (int j = 0; j < 4; j++) {
+        int sum = 0;
+        bool hasNeg = false;
+        for (int i = 0; i < 3; i++) {
+            if (m[i][j] < 0) {
+                sum += m[i][j];
+                hasNeg = true;
+            }
+        }
+        if (hasNeg && sum > bsum) {
+            bsum = sum;
+            stolb = j;
+        }
+    }
+
+    // Обнуляем найденный столбец
+    if (stolb != -1) {
+        for (int i = 0; i < 3; i++) {
+            m[i][stolb] = 0;
+        }
+    }
+
+    // Вывод матрицы
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
